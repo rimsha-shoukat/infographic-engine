1045026a38a1a1de6b709a5b0b4f3ac1f2e0a41a
rimsha-shoukat <rimsha.devs@gmail.com>
2026-03-29 11:45:25 +0500

    filter functionality added

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,14 @@
 function scrollToStories() {
     document.querySelector("#stories").scrollIntoView({ behavior: "smooth" });
 }
+
+function filterStories(type) {
+    const cards = document.querySelectorAll("#storyCard");
+    cards.forEach(card => {
+        if (type === "all" || type === card.getAttribute('data-type')) {
+            card.style.display = "flex";
+        } else {
+            card.style.display = "none";
+        }
+    })
+}
