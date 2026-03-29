66bd682ae97b31051c041da2b30b86d50e238c31
rimsha-shoukat <rimsha.devs@gmail.com>
2026-03-29 15:31:21 +0500

    infographic dynamic page added

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,6 @@
 function scrollToStories() {
     document.querySelector("#stories").scrollIntoView({ behavior: "smooth" });
-}
+};
 
 function filterStories(type) {
     const cards = document.querySelectorAll("#storyCard");
@@ -11,4 +11,17 @@ function filterStories(type) {
             card.style.display = "none";
         }
     })
-}
+};
+
+function changeChart(type) {
+    const buttons = document.querySelectorAll("#chartBtn");
+    buttons.forEach(btn => {
+        if (btn.getAttribute("data-type") === type) {
+            btn.classList.add("bg-black");
+            btn.classList.remove("bg-transparent");
+        } else {
+            btn.classList.add("bg-transparent");
+            btn.classList.remove("bg-black");
+        }
+    })
+};
--- routes/infographic.js
+++ routes/infographic.js
@@ -5,7 +5,7 @@ const data = require("../data/infographics.json");
 
 router.get("/:id", (req, res) => {
     const item = data.find(i => i.id === req.params.id);
-        res.render("infographic", { item });
+    res.render("infographic", { data, item });
 });
 
 module.exports = router;
